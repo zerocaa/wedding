1270dbc90c83c3c1d9f87600cdbb19c4d70a7fe0
hà huy hùng <83862705+ZeroRyo@users.noreply.github.com>
2022-02-28 09:37:23 +0700

    initialize the homepage interface
    Co-Authored-By: baoanh2809 <88428091+baoanh2809@users.noreply.github.com>
    Co-Authored-By: nhanVietnam <62474480+nhanVietnam@users.noreply.github.com>

---
+++ static/js/themee030.js
@@ -0,0 +1,42 @@
+function message(title, message) {
+	$("#messageModal .messageTitle").html(title);
+	$("#messageModal .messageBody").html(message);
+	$('#messageModal').modal('show');
+}
+function uuidv4() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	});
+}
+function setFormErrors(formElement, errors) {
+	$.each(errors, function( key, value ) {
+		formElement.find('#' + key).parent().addClass('has-error').find('.help-block').html(value[0]);
+	});
+}
+
+function unsetFormErrors(formElement) {
+	formElement.find('.has-error').removeClass('has-error');
+	formElement.find('.help-block').html('');
+	formElement.find('.form-message').hide();
+}
+
+$(document).ready(function(){
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl,{trigger:'hover'})
+    });
+
+	$(document).on("change", ".date-custom input[type='date']", function() {
+		this.setAttribute("data-date",moment(this.value, "YYYY-MM-DD").format( this.getAttribute("data-date-format")));
+	}).trigger("change");
+
+	$(document).on("click", ".btn-resend-verify-email", function(){
+		$(this).addClass("pe-none");
+		$(".loading").show();
+		$("form#resend-verify-email-form").submit();
+	});
+	if(document.getElementById("typed1")){
+		var typed1 = new Typed('#typed1', $('#typed1').data('typed'));	
+	}
+});
