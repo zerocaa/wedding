b3c4ff866be1f2df123b0e50707ead4cfc0774e9
baoanh2809 <88428091+baoanh2809@users.noreply.github.com>
2022-06-06 10:40:02 +0700

    BAN GOC
    This reverts commit b0ecbbb2898d27633358b82b5863e4f9a4dab1f6.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const http = require('http');
-
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
