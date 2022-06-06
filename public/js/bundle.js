d8efd194ff9f80aea017d2f5b99c5b2986ef3b4e
baoanh2809 <88428091+baoanh2809@users.noreply.github.com>
2022-06-06 09:57:49 +0700

    Revert "thay link http"
    This reverts commit 88894a28322ca72a2b0e452bfd892c01da5a33e5.

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8875,7 +8875,7 @@ var login = /*#__PURE__*/function () {
             _context.next = 4;
             return (0, _axios.default)({
               method: 'POST',
-              url: 'https://wedding-production-09d7.up.railway.app/api/v1/users/login',
+              url: 'http://localhost:3000/api/v1/users/login',
               data: {
                 email: email,
                 password: password
@@ -8932,7 +8932,7 @@ var logout = /*#__PURE__*/function () {
 
           case 3:
             res = _context2.sent;
-            if (res.data.status = 'success') location.reload(true);
+            if (res.data.status = "success") location.reload(true);
             (0, _alert.showAlert)('success', 'logout success');
             _context2.next = 11;
             break;
@@ -10520,7 +10520,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50382" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55900" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
