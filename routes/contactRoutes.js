c8d2e952e5de3773c477883541b907b3954ac9c4
baoanh2809 <88428091+baoanh2809@users.noreply.github.com>
2022-06-17 23:29:42 +0700

    Revert "fix lỗi"
    This reverts commit b6570829278315dfc76cd55a3749d18d4ea7dc2a.

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const contact = require('../controllers/ContactController');
+const contact = require('../controllers/contactController');
 
 router
   .route('/')
